5ccd8cf27ea0f9fa00f595284d1cb9a50b58b2c5
Ryan McEntire <107779145+RyanMcEntire@users.noreply.github.com>
2023-03-07 15:19:35 -0700

    add content for menu page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import "./style.css";
 import { mainHeader, headerNav } from "./header.js";
 import { home } from "./home/home.js";
+import { menu } from "./menu/menu.js";
 
 mainHeader();
 headerNav();
-home();
+menu();
+// home();
 
 
 console.log("cosnole lot dog");
--- src/menu/menu.js
+++ src/menu/menu.js
@@ -1,3 +1,30 @@
 const content = document.querySelector("#content");
-const content = document.querySelector("#content");
-const menuButton = document.querySelector("#menuButton");
+
+const menu = () => {
+  const menuContainer = document.createElement("div");
+  menuContainer.classList.add("menuContainer");
+
+  const fullMenu = document.createElement("div");
+  fullMenu.setAttribute("id", "fullMenu");
+  const fullMenuTitle = document.createElement("h2");
+  fullMenuTitle.textContent = "Full Menu";
+  fullMenu.appendChild(fullMenuTitle);
+  const menuContent = document.createElement("div");
+  menuContent.textContent =
+    "Its pretty self explanatory, if ya think about it. We serve the finest Full Potato, our specialty. We don't need anything too fancy here at The Full Potato, and if you're anything like us, we'd like to have ya try it.";
+  fullMenu.appendChild(menuContent);
+  menuContainer.appendChild(fullMenu);
+  const menuImage = document.createElement("img");
+  menuImage.classList.add("menuImage");
+  menuImage.src = "../src/menu/plain-potato.jpg"; 
+  menuContainer.appendChild(menuImage);
+
+  content.appendChild(menuContainer);
+
+  const menuButton = document.querySelector("#menuButton");
+  menuButton.classList.add("currentTab");
+
+  return menuContainer;
+};
+
+export { menu };
